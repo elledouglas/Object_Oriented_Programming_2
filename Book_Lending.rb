903c8c5d14bdfd84458b91362d03bfcb3205b3b1
Kathleen <kathleenmcclay@gmail.com>
2017-07-04 15:57:02 -0400

    Book_Lending complete

--- Book_Lending.rb
+++ Book_Lending.rb
@@ -2,8 +2,6 @@ class Book
   attr_accessor :due_date
 
 
-
-
   def intitialize
     @@on_shelf = []
     @@on_loan = []
@@ -11,88 +9,86 @@ class Book
 
 end
 
+
+def self.create(title, author, isbn)
+  @book = [title, author, isbn]
+  @@on_shelf << @book
+
+  return @book
+end
+
   def available
   @@on_shelf
+  return true
 end
 
   def borrowed
  @@on_loan
+
 end
 
 
-  def borrow(book)
+ def browse
+  @book.sample
+ end
+
+ def lent_out?
+   if @@on_shelf == true
+     return false
+
+   else @@on_shelf == false
+     return true
+ end
+end
+
+
+  def borrow
 
     if lent_out? == true
     puts "Your book is unavailable"
 
-    elsif  lent_out? == false
+    elsif lent_out? == false
     current_due_date = Time.now #add time
     @@on_shelf.delete #
-    @@on_loan << book
+    @@on_loan << @book
     return true
 
+  end
 end
 
 
   def due_date
-      Time.now +30
+      Time.now + 3000
   end
 
 
-  def return_to_library(book)
+  def return_to_library
 
   if lent_out? == false
     return false
 
   elsif
     @@on_loan.delete
-    @@on_shelf << book
-    due_date = nil
+    @@on_shelf << @book
+    due_date.new = nil
     return true
 
   end
 end
 
 
-    def lent_out?
-
-    if borrow == true
-      puts "The book is currently unavailbe"
-
-    elsif "The book is available"
-  end
-
 
 #class Methodas
-  def self.create(title, author, isbn)
-    book = [@title, @author, @isbn]
-    @@on_shelf << book
-
-    return new_book
-  end
-
 
 
   def current_due_date
     Time.now
-  end
-
-
-def overdue_books
-  due_date <= Time.now
-  return book
 end
 
-
-def browse
-   #random book
+  def overdue_books
+    due_date <= Time.now
+  return @book
 end
-
-
-
-
-
-
 
 
 end
